a71598b7e7f0cd774f9ee8f02007955ff24be02c
itallovidal <itallovidalll@gmail.com>
2024-01-16 08:59:50 -0300

    fc

--- src/main/java/apisistemacadastramento/apisc/categoryController/CategoryController.java
+++ src/main/java/apisistemacadastramento/apisc/categoryController/CategoryController.java
@@ -1,2 +1,22 @@
-package apisistemacadastramento.apisc.categoryController;public class CategoryController {
+package apisistemacadastramento.apisc.categoryController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+
+    @GetMapping()
+    public ResponseEntity<Object> getCategories(){
+        return ResponseEntity.status(200).build();
+    }
+
+    @PutMapping("create")
+    public ResponseEntity<Object> createCategory(){
+        return ResponseEntity.status(201).build();
+    }
 }
--- src/main/java/apisistemacadastramento/apisc/productController/ProductController.java
+++ src/main/java/apisistemacadastramento/apisc/productController/ProductController.java
@@ -9,9 +9,28 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("products")
 public class ProductController {
 
-    @PostMapping("/create")
-    public ResponseEntity<Product> hello(@RequestBody @Valid Product  product){
+    @PutMapping("/create")
+    public ResponseEntity<Product> create(@RequestBody @Valid Product  product){
         return ResponseEntity.status(201).body(product);
     }
 
+    @GetMapping("/details/{id}")
+    public ResponseEntity<String> getProductDetails(@PathVariable("id") String id){
+        return ResponseEntity.status(201).body(id);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<String> searchProduct(@RequestParam(value = "q") String  query){
+        return ResponseEntity.status(201).body(query);
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable("id") String id){
+        return ResponseEntity.status(201).body(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("id")  String id){
+        return ResponseEntity.status(201).body(id);
+    }
 }
